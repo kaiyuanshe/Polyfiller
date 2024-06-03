486c86b202a00027039311b637207155caf0c597
luojiyin <luojiyin@hotmail.com>
2024-06-03 18:32:46 +0800

    umount before start docker

--- start-docker.sh
+++ start-docker.sh
@@ -1,9 +1,18 @@
 #! /usr/bin/env bash
 
-docker load < $1
+docker load <$1
 docker image ls -a
 
-docker-compose down -v --remove-orphans
-docker-compose up -d
+# get the current working directory
+current_dir=$(pwd)
+
+# get polyfill-cache full path
+mount_point="${current_dir}/polyfill-cache"
+
+# umount polyfill-cache dir
+sudo umount "$mount_point"
+
+docker compose down -v --remove-orphans
+docker compose up -d
 
 docker image prune -a -f
