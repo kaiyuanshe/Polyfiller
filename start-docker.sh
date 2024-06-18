d83145f79f0ad762c2c3fafb12bd7cd89d2b513f
luojiyin <luojiyin@hotmail.com>
2024-06-18 14:57:23 +0800

    code clear

--- start-docker.sh
+++ start-docker.sh
@@ -3,11 +3,8 @@
 docker load <$1
 docker image ls -a
 
-# get polyfill-cache full path
-mount_point="$(pwd)/polyfill-cache"
-
 # umount polyfill-cache dir
-sudo umount "$mount_point"
+sudo umount "$(pwd)/polyfill-cache"
 
 docker compose down -v --remove-orphans
 docker compose up -d
