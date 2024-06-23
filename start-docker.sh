be0d57822c5c38401fce3aed5a5bef73ef6cf7df
luo jiyin <luojiyin@hotmail.com>
2024-06-23 09:05:35 +0800

    get config from service-configuartion (#7)
    Co-authored-by: South Drifted <shiy2008@gmail.com>

--- start-docker.sh
+++ start-docker.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
 
-docker load < $1
+docker load <$1
 docker image ls -a
 
-docker-compose down -v --remove-orphans
-docker-compose up -d
+# umount polyfill-cache dir
+sudo umount "$(pwd)/polyfill-cache" || true
+
+docker compose down -v --remove-orphans
+docker compose up -d
 
 docker image prune -a -f
