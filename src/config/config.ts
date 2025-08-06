5eec0847f44b3b5b3cc4209e2f931d3ac38e0199
luojiyin <luojiyin@hotmail.com>
2025-08-06 23:27:53 +0800

    refactor: config

--- src/config/config.ts
+++ src/config/config.ts
@@ -21,6 +21,7 @@ export interface Config {
 	key: Buffer | undefined;
 	cert: Buffer | undefined;
 	// S3 storage
+	enableS3Storage: boolean;
 	s3Storage?: Record<"region"| "bucket" | "accessKeyId" | "secretAccessKey" | "endpoint", string> & {
 		forcePathStyle?: boolean;
 	};
@@ -28,37 +29,38 @@ export interface Config {
 
 export const config: Config = {
 	version: generateReleaseName(pkg),
-	sentryDsn: environment.SENTRY_DSN,
-	environment: environment.NODE_ENV,
-	production: environment.NODE_ENV != null && environment.NODE_ENV.toLowerCase() === "production",
-	testing: booleanize(environment.TESTING),
-	volumes: environment.VOLUMES.split(" ")
-		.map(part => part.trim())
-		.filter(part => part.length > 0),
-	logLevel: parseLogLevel(environment.LOG_LEVEL) ?? "info",
-	clearCache: booleanize(environment.CLEAR_CACHE),
-	https: booleanize(environment.HTTPS),
-	host: environment.HOST,
-	port: parseInt(environment.PORT),
+	sentryDsn: environment.SENTRY_DSN as string | undefined,
+	environment: environment.NODE_ENV as string,
+	production: environment.NODE_ENV != null && (environment.NODE_ENV as string).toLowerCase() === "production",
+	testing: booleanize(environment.TESTING as string | undefined),
+	volumes: (environment.VOLUMES as string).split(" ")
+		.map((part: string) => part.trim())
+		.filter((part: string) => part.length > 0),
+	logLevel: parseLogLevel(environment.LOG_LEVEL as string) ?? "info",
+	clearCache: booleanize(environment.CLEAR_CACHE as string | undefined),
+	https: booleanize(environment.HTTPS as string | undefined),
+	host: environment.HOST as string,
+	port: parseInt(environment.PORT as string),
 	key:
 		environment.KEY == null || environment.KEY === ""
 			? undefined
-			: environment.KEY.trim().startsWith("-----BEGIN RSA PRIVATE KEY-----")
-			? Buffer.from(environment.KEY.replace(/\\n/g, "\n"))
-			: readFileSync(environment.KEY),
+			: (environment.KEY as string).trim().startsWith("-----BEGIN RSA PRIVATE KEY-----")
+			? Buffer.from((environment.KEY as string).replace(/\\n/g, "\n"))
+			: readFileSync(environment.KEY as string),
 	cert:
 		environment.CERT == null || environment.CERT === ""
 			? undefined
-			: environment.CERT.trim().startsWith("-----BEGIN CERTIFICATE-----")
-			? Buffer.from(environment.CERT.replace(/\\n/g, "\n"))
-			: readFileSync(environment.CERT),
+			: (environment.CERT as string).trim().startsWith("-----BEGIN CERTIFICATE-----")
+			? Buffer.from((environment.CERT as string).replace(/\\n/g, "\n"))
+			: readFileSync(environment.CERT as string),
 	// S3 config
+	enableS3Storage: booleanize(environment.ENABLE_S3_STORAGE as string | undefined),
 	s3Storage: {
-		region: environment.S3_REGION ?? "",
-		bucket: environment.S3_BUCKET ?? "",
-		accessKeyId: environment.S3_ACCESS_KEY_ID ?? "",
-		secretAccessKey: environment.S3_SECRET_ACCESS_KEY ?? "",
-		endpoint: environment.S3_ENDPOINT ?? "",
-		forcePathStyle: booleanize(environment.S3_FORCE_PATH_STYLE)
+		region: environment.S3_REGION as string ?? "",
+		bucket: environment.S3_BUCKET as string ?? "",
+		accessKeyId: environment.S3_ACCESS_KEY_ID as string ?? "",
+		secretAccessKey: environment.S3_SECRET_ACCESS_KEY as string ?? "",
+		endpoint: environment.S3_ENDPOINT as string ?? "",
+		forcePathStyle: booleanize(environment.S3_FORCE_PATH_STYLE as string | undefined)
 	}
 };
