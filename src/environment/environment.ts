a2b4e0c3104451cee260a0c061f08251cf598d64
luojiyin <luojiyin@hotmail.com>
2025-08-06 21:53:52 +0800

    code clear

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,5 @@
-import {config as dotenvConfig} from "dotenv";
+import "dotenv/config";
 import {uppercaseKeys} from "../api/util.js";
 
-// 加载 .env 文件
-dotenvConfig();
-
+// load .env file
 export const environment = uppercaseKeys(process.env as Record<string, unknown>);
