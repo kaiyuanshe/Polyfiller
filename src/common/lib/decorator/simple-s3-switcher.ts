f423623fd3cd57eb01fa0750122b72baa20d0379
luojiyin <luojiyin@hotmail.com>
2025-08-07 16:27:46 +0800

    code clear

--- src/common/lib/decorator/simple-s3-switcher.ts
+++ src/common/lib/decorator/simple-s3-switcher.ts
@@ -2,14 +2,14 @@
  * S3 method switcher decorator
  * If the path is S3 path, use the current method, otherwise call the parent class method
  */
-export function S3OrLocal<T extends { isS3Path(path: string): boolean }>(
+export function fallbackSuper<T extends { isValidPath(path: string): boolean }>(
 	target: (this: T, path: string, ...args: any[]) => any,
 	context: ClassMethodDecoratorContext<T>
 ) {
 	const methodName = context.name as string;
 	
 	return function (this: T, path: string, ...args: any[]) {
-		if (this.isS3Path(path)) {
+		if (this.isValidPath(path)) {
 			// Use the current class method (S3 implementation)
 			return target.call(this, path, ...args);
 		} else {
