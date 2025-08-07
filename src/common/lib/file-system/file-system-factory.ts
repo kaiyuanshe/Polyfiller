8ad9c5735b34d049f977cd9a3d7b7f5cf477895f
luojiyin <luojiyin@hotmail.com>
2025-08-07 16:23:15 +0800

    code clear

--- src/common/lib/file-system/file-system-factory.ts
+++ src/common/lib/file-system/file-system-factory.ts
@@ -1,12 +1,6 @@
-import type {FileSystem} from "./file-system";
 import {realFileSystem} from "./real-file-system";
 import {S3FileSystem} from "./s3-file-system";
 import type {Config} from "../../../config/config";
 import type {ILoggerService} from "../../../service/logger/i-logger-service";
 
-export function createFileSystem(config: Config, logger: ILoggerService): FileSystem {
-	if (config.enableS3Storage) {
-		return new S3FileSystem(config, logger);
-	}
-	return realFileSystem;
-} 
+export const createFileSystem = (config: Config, logger: ILoggerService) => (config.enableS3Storage ? new S3FileSystem(config, logger) : realFileSystem);
